2bf221486984e86c5d47bda17b78930a4e9d349a
Fathul Muiin <fmuiin14@gmail.com>
2019-06-23 18:29:49 +0700

    homestayapp version 1

---
+++ build/precache-manifest.6c6a5b3312f8f78bb75fb70095d392cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166dfbd1c59b381ff2432916a71423a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e411fb680729ada5a86",
+    "url": "/static/css/main.b966f83b.chunk.css"
+  },
+  {
+    "revision": "51a76e3251bd579d69c9",
+    "url": "/static/js/2.95549aa8.chunk.js"
+  },
+  {
+    "revision": "1e411fb680729ada5a86",
+    "url": "/static/js/main.a822e00b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
